72f4f93f8cd73495a001ba048dbae79037087274
kosty <you@example.com>
2022-01-30 15:49:41 +0300

    add permission_classes to @action

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, generics, decorators
+from rest_framework import viewsets, status, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
